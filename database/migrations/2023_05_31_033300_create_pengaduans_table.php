37559b17d4710f8c0fc75c42fdc631611debf50a
Fauzan <ojanaja03@gmail.com>
2023-06-27 19:20:49 +0700

    new update db

--- database/migrations/2023_05_31_033300_create_pengaduans_table.php
+++ database/migrations/2023_05_31_033300_create_pengaduans_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,12 +14,14 @@ return new class extends Migration
             $table->id('id_pengaduan');
             $table->dateTime('tgl_pengaduan');
             $table->char('nik', 16);
+            $table->text('kategori_laporan');
+            $table->text('judul_laporan');
             $table->text('isi_laporan');
             $table->string('foto_laporan');
             $table->enum('status', ['0', 'proses', 'selesai']);
 
-
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP(0)'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP(0)'));
 
             $table->foreign('nik')->references('nik')->on('masyarakat');
         });
